686dbf2ceb79c7a40f4ec8521f1a325658419a29
Automated Edits <devnull@example.com>
2026-06-19 01:22:12 +0300

    update the yellow color for light theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-const kYellow = Color(0xFFFADA5E);
+const kYellow = Color(0xFFE8CE3F);
 const kBlack = Color(0xFF000000);
 // Primary text color used on light backgrounds
 const kCharcoal = Color(0xFF191716);
